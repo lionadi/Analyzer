6dacc94401ca948369f05625abb640ffcbf6493f
aDI <aDI@DESKTOP-8VKI8KB>
2017-02-15 19:45:09 +0200

    Improved parallel processing

--- WebCrawler/WebScraperService.cs
+++ WebCrawler/WebScraperService.cs
@@ -136,139 +136,160 @@ namespace Analyzer.WebCrawler
             // We don't want to cause problems for the site and do not want our function to be interupted.
             // NOTICE: This might not work if you are using a web service, this is presently designed for web URLs
             List<RSS.FeedItem> rssFeedsWebLocationsToProcess = new List<RSS.FeedItem>();
+            Task.Factory.StartNew(() =>
+           {
+               var rssFeeds = this.webSources.Where(o => o.CrawlerType == CrawlerType.RSS || o.CrawlerType == CrawlerType.RSSContentNoLinkFollowUp);
 
-            var rssFeeds = this.webSources.Where(o => o.CrawlerType == CrawlerType.RSS || o.CrawlerType == CrawlerType.RSSContentNoLinkFollowUp);
+               foreach (var rssFeed in rssFeeds)
+               {
+                   Task.Factory.StartNew(() => ProcessRSSFeed(rssFeed, rssFeedsWebLocationsToProcess), TaskCreationOptions.AttachedToParent);
+               }
+           }, TaskCreationOptions.LongRunning);
 
-            foreach (var rssFeed in rssFeeds)
+            // Create a new thread that is long running and processes multiple RSS items
+            Task.Factory.StartNew(() =>
             {
-                // Check to see if the scraping is to be stopped
-                if (!this.isServiceScraping)
-                    break;
-
-                var rssItems = RSSCrawler.ProcessRSSFeed(rssFeed.URL, ConfigurationManager.AppSettings.RSSFeedHistoryRangeForScrapingInDays);
-                if (rssItems != null)
+                // Shuftle the location to get a randomized processing queue, helps making a more clear and reliable code and avoid being banned or causing harm to the scraped site
+                rssFeedsWebLocationsToProcess.Shuffle();
+                // Process the web location queue
+                foreach (var rssItem in rssFeedsWebLocationsToProcess)
                 {
-                    foreach (var rssItem in rssItems)
+                    Task.Factory.StartNew(() => ProcessRSSItem(rssItem), TaskCreationOptions.AttachedToParent);
+                }
+            }, TaskCreationOptions.LongRunning);
+            Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("ENDED: RSS Feed scraping iteration started at: " + DateTime.Now);
+        }
+
+        private void ProcessRSSFeed(Source rssFeed, List<RSS.FeedItem> rssFeedsWebLocationsToProcess)
+        {
+            // Check to see if the scraping is to be stopped
+            if (!this.isServiceScraping)
+                return;
+
+            var rssItems = RSSCrawler.ProcessRSSFeed(rssFeed.URL, ConfigurationManager.AppSettings.RSSFeedHistoryRangeForScrapingInDays);
+            if (rssItems != null)
+            {
+                foreach (var rssItem in rssItems)
+                {
+                    // Check to see if the scraping is to be stopped
+                    if (!this.isServiceScraping)
+                        break;
+
+                    // Skip this item if it has been processed earlier
+                    if (rssItem.Published <= rssFeed.LastRunTime)
+                        continue;
+
+                    rssItem.SourceType = rssFeed.SourceType;
+                    rssItem.Category = rssFeed.Category;
+                    //rssItem.ProcessingTimeLimit = rssFeed.LastRunTime;
+
+                    // Check to see if only the RSS feed data is to be saved and not the actual data behing the feed item
+                    if (rssFeed.CrawlerType == CrawlerType.RSSContentNoLinkFollowUp)
                     {
-                        // Check to see if the scraping is to be stopped
-                        if (!this.isServiceScraping)
-                            break;
-
-                        // Skip this item if it has been processed earlier
-                        if (rssItem.Published <= rssFeed.LastRunTime)
-                            continue;
-
-                        rssItem.SourceType = rssFeed.SourceType;
-                        rssItem.Category = rssFeed.Category;
-                        //rssItem.ProcessingTimeLimit = rssFeed.LastRunTime;
-
-                        // Check to see if only the RSS feed data is to be saved and not the actual data behing the feed item
-                        if (rssFeed.CrawlerType == CrawlerType.RSSContentNoLinkFollowUp)
+                        var result = Analyzer.Common.Database.DatabaseService.GetInstance().AddtoWriteQueueAsync<Analyzer.Common.Database.DataItems.WebData>(rssItem.SourceType.ToString(), rssItem.ToWebData());
+                        if (!result.Result)
                         {
-                            var result = Analyzer.Common.Database.DatabaseService.GetInstance().AddtoWriteQueueAsync<Analyzer.Common.Database.DataItems.WebData>(rssItem.SourceType.ToString(), rssItem.ToWebData());
-                            if (!result)
-                            {
-                                Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to add the rss item to the database queue: " + rssItem.Url + " from feed: " + rssFeed.URL);
-                                this.itemsFailedCounter++;
-                            }
-                            else
-                            {
-                                this.itemsProcessedCounter++;
-                            }
+                            Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to add the rss item to the database queue: " + rssItem.Url + " from feed: " + rssFeed.URL);
+                            this.itemsFailedCounter++;
                         }
                         else
                         {
-                            // Add the RSS item to be processed as a normal web page, follow the RSS item link
-                            rssFeedsWebLocationsToProcess.Add(rssItem);
+                            this.itemsProcessedCounter++;
                         }
                     }
+                    else
+                    {
+                        // Add the RSS item to be processed as a normal web page, follow the RSS item link
+                        rssFeedsWebLocationsToProcess.Add(rssItem);
+                    }
                 }
-                else
-                {
-                    Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to process rss feed: " + rssFeed.URL);
-                }
-
-                rssFeed.LastRunTime = DateTime.Now;
-                // avoid being banned or causing harm to the scraped site, being nice :)
-                System.Threading.Thread.Sleep(Analyzer.Common.Configuration.ConfigurationManager.AppSettings.RSSFeedReaderWaitTimeInMilliseconds);
+            }
+            else
+            {
+                Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to process rss feed: " + rssFeed.URL);
             }
 
-            // Shuftle the location to get a randomized processing queue, helps making a more clear and reliable code and avoid being banned or causing harm to the scraped site
-            rssFeedsWebLocationsToProcess.Shuffle();
-            // Process the web location queue
-            foreach (var rssItem in rssFeedsWebLocationsToProcess)
+            rssFeed.LastRunTime = DateTime.Now;
+            // avoid being banned or causing harm to the scraped site, being nice :)
+            System.Threading.Thread.Sleep(Analyzer.Common.Configuration.ConfigurationManager.AppSettings.RSSFeedReaderWaitTimeInMilliseconds);
+        }
+
+        private void ProcessRSSItem(FeedItem rssItem)
+        {
+            // Check to see if the scraping is to be stopped
+            if (!this.isServiceScraping)
+                return;
+
+            // Skip this item if it has been processed earlier
+            //if (rssItem.Published <= rssItem.ProcessingTimeLimit)
+            //    continue;
+
+            PageItem webLocationData = null;
+
+            switch (rssItem.SourceType)
             {
-                // Check to see if the scraping is to be stopped
-                if (!this.isServiceScraping)
+                case SourceType.WordPress:
+                    {
+                        webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWordpressArticle(rssItem.Url);
+                    }
                     break;
 
-                // Skip this item if it has been processed earlier
-                //if (rssItem.Published <= rssItem.ProcessingTimeLimit)
-                //    continue;
-
-                PageItem webLocationData = null;
-
-                switch(rssItem.SourceType)
-                {
-                    case SourceType.WordPress:
-                        {
-                            webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWordpressArticle(rssItem.Url);
-                        } break;
-
-                    case SourceType.Facebook:
-                        {
-                            throw new NotImplementedException();
-                        } break;
-
-                    case SourceType.Twitter:
-                        {
-                            throw new NotImplementedException();
-                        }
-                        break;
-
-                    case SourceType.Common:
-                        {
-                            webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url);
-                        }
-                        break;
-
-                    case SourceType.ScienceDaily:
-                        {
-                            webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url, "#story_text");
-                        } break;
-
-                    default:
-                        {
-                            webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url);
-                        } break;
-                }
-                
-                if (webLocationData != null)
-                {
-                    webLocationData.Category = rssItem.Category;
-                    webLocationData.Published = rssItem.Published;
-                    if (String.IsNullOrEmpty(webLocationData.Title))
-                        webLocationData.Title = rssItem.Title;
-                    var result = Analyzer.Common.Database.DatabaseService.GetInstance().AddtoWriteQueueAsync<Analyzer.Common.Database.DataItems.WebData>(rssItem.SourceType.ToString(), webLocationData.ToWebData());
-                    if (!result)
+                case SourceType.Facebook:
                     {
-                        Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to add the web location to the database queue: " + rssItem.Url);
-                        this.itemsFailedCounter++;
-                    } else
-                    {
-                        this.itemsProcessedCounter++;
+                        throw new NotImplementedException();
                     }
+                    break;
+
+                case SourceType.Twitter:
+                    {
+                        throw new NotImplementedException();
+                    }
+                    break;
+
+                case SourceType.Common:
+                    {
+                        webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url);
+                    }
+                    break;
+
+                case SourceType.ScienceDaily:
+                    {
+                        webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url, "#story_text");
+                    }
+                    break;
+
+                default:
+                    {
+                        webLocationData = Analyzer.WebCrawler.Web.WebCrawler.ProcessWebPage(rssItem.Url);
+                    }
+                    break;
+            }
+
+            if (webLocationData != null)
+            {
+                webLocationData.Category = rssItem.Category;
+                webLocationData.Published = rssItem.Published;
+                if (String.IsNullOrEmpty(webLocationData.Title))
+                    webLocationData.Title = rssItem.Title;
+                var result = Analyzer.Common.Database.DatabaseService.GetInstance().AddtoWriteQueueAsync<Analyzer.Common.Database.DataItems.WebData>(rssItem.SourceType.ToString(), webLocationData.ToWebData());
+                if (!result.Result)
+                {
+                    Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to add the web location to the database queue: " + rssItem.Url);
+                    this.itemsFailedCounter++;
                 }
                 else
                 {
-                    Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to process web location: " + rssItem.Url);
-                    this.itemsFailedCounter++;
+                    this.itemsProcessedCounter++;
                 }
-
-                // avoid being banned or causing harm to the scraped site, being nice :)
-                System.Threading.Thread.Sleep(Analyzer.Common.Configuration.ConfigurationManager.AppSettings.WebDataReaderWaitTimeInMilliseconds);
             }
-            Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("ENDED: RSS Feed scraping iteration started at: " + DateTime.Now);
-        }
+            else
+            {
+                Analyzer.Common.Logger.ExceptionLoggingService.Instance.WriteWebScrapingInformation("WARNING: Unable to process web location: " + rssItem.Url);
+                this.itemsFailedCounter++;
+            }
+
+            // avoid being banned or causing harm to the scraped site, being nice :)
+            System.Threading.Thread.Sleep(Analyzer.Common.Configuration.ConfigurationManager.AppSettings.WebDataReaderWaitTimeInMilliseconds);
+        } 
     }
 }
